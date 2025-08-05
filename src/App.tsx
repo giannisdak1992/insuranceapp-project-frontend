41736eaddfaf382de9230d055be770d5f450aefa
Yiannis Ntakoumis <giannisdak@gmail.com>
2025-08-05 09:57:01 +0300

    feat: adding InsurancePolicyInsertPage and InsurancePolicyList in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,31 @@ import CustomerInsertFormPage from "./pages/CustomerInsertFormPage";
 import CustomerEditFormPage from "./pages/CustomerEditFormPage";
 import CarInsertFormPage from "@/pages/CarInsertFormPage.tsx";
 import CarListPage from "@/pages/CarListPage.tsx";
+import InsurancePolicyInsertFormPage from "@/pages/InsurancePolicyInsertFormPage.tsx";
+import InsurancePolicyListPage from "@/pages/InsurancePolicyListPage.tsx";
 
 function App() {
     return (
         <Router>
             <nav className="bg-blue-600 p-4 text-white flex space-x-4">
                 <Link to="/" className="hover:underline">Customers</Link>
-                <Link to="/add" className="hover:underline">Add a new Customer</Link>
+                <Link to="/add-customer" className="hover:underline">Add a new Customer</Link>
                 <Link to = "/add-car"  className="hover:underline">Add a new Car</Link>
                 <Link to = "/cars" className="hover:underline">Cars</Link>
+                <Link to = "/policies" className="hover:underline">Policies</Link>
+                <Link to = "/add-insurance-policy" className="hover:underline">Add an Insurance Policy</Link>
             </nav>
 
             <main className="p-4">
                 <Routes>
                     <Route path="/" element={<CustomerListPage />} />
-                    <Route path="/add" element={<CustomerInsertFormPage />} />
+                    <Route path="/add-customer" element={<CustomerInsertFormPage />} />
                     <Route path="/customers/edit/:id" element={<CustomerEditFormPage />} />
                     <Route path = "/add-car" element={<CarInsertFormPage />} />
                     <Route path = "/cars" element={<CarListPage />} />
+                    <Route path = "/add-insurance-policy" element={<InsurancePolicyInsertFormPage/>} />
+                    <Route path = "/policies" element={<InsurancePolicyListPage/>} />
+
                 </Routes>
             </main>
         </Router>
