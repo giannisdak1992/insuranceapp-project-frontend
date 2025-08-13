281b8976db7087e7e3233086a756d3716d9129dd
Yiannis Ntakoumis <giannisdak@gmail.com>
2025-08-13 15:22:37 +0300

    feat: add AuthProvider and embed LoginFormPage in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "@/layout/MainLayout";
-
 import CustomerListPage from "@/pages/CustomerListPage";
 import CustomerInsertFormPage from "@/pages/CustomerInsertFormPage";
 import CustomerEditFormPage from "@/pages/CustomerEditFormPage";
@@ -9,28 +8,33 @@ import CarListPage from "@/pages/CarListPage";
 import InsurancePolicyInsertFormPage from "@/pages/InsurancePolicyInsertFormPage";
 import InsurancePolicyListPage from "@/pages/InsurancePolicyListPage";
 import CustomerPoliciesPage from "@/pages/CustomerPoliciesPage";
-
+import {AuthProvider} from "@/context/AuthProvider.tsx";
+import LoginFormPage from "@/pages/LoginFormPage.tsx";
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <MainLayout  />
-                    }
-                >
-                    <Route index element={<CustomerListPage />} />
-                    <Route path="add-customer" element={<CustomerInsertFormPage />} />
-                    <Route path="customers/edit/:id" element={<CustomerEditFormPage />} />
-                    <Route path="add-car" element={<CarInsertFormPage />} />
-                    <Route path="cars" element={<CarListPage />} />
-                    <Route path="add-insurance-policy" element={<InsurancePolicyInsertFormPage />} />
-                    <Route path="policies" element={<InsurancePolicyListPage />} />
-                    <Route path="my-policies" element={<CustomerPoliciesPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <MainLayout  />
+                        }
+                    >
+                        <Route path="login" element={<LoginFormPage />} />
+                        <Route index element={<CustomerListPage />} />
+                        <Route path="add-customer" element={<CustomerInsertFormPage />} />
+                        <Route path="customers/edit/:id" element={<CustomerEditFormPage />} />
+                        <Route path="add-car" element={<CarInsertFormPage />} />
+                        <Route path="cars" element={<CarListPage />} />
+                        <Route path="add-insurance-policy" element={<InsurancePolicyInsertFormPage />} />
+                        <Route path="policies" element={<InsurancePolicyListPage />} />
+                        <Route path="my-policies" element={<CustomerPoliciesPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+
     );
 }
 
